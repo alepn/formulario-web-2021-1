c0380f3912ce92138710447f2f682a62784be1ae
Alexandre <alexandre.pnorb@gmail.com>
2021-03-22 22:38:03 -0300

    Envio de formulário com Javascript

--- funcoes.js
+++ funcoes.js
@@ -1,6 +1,35 @@
+function validarFormulario(formulario) {
+
+    for(i=0; i<formulario.length-2; i++){
+        if(formulario[i].value == ''){
+            return false;
+        }
+    }
+
+    return true;
+
+}
+
 function enviarFormulario(formulario) {   
 
-    document.getElementById("result-form").innerHTML += formulario.nome.value;
+    var validacao = validarFormulario(formulario);
+
+    if(validacao) {
+        document.querySelector("tbody").innerHTML += `\
+        <tr>\
+        <td>${formulario.nome.value}</td>\
+        <td>${formulario.email.value}</td>\
+        <td>${formulario.telefone.value}</td>\
+        </tr>\
+        `;
+        
+        document.querySelector("table").setAttribute('class', '');
+
+        formulario.reset();
+    }
+    else{
+        alert("Preencha os campos do formulário!");
+    }
 
     return false;
 }
