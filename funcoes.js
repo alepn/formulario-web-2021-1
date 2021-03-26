35d4c4995a574a74e6204c799691af532ea7d686
Alexandre <alexandre.pnorb@gmail.com>
2021-03-26 20:33:22 -0300

    Exemplo JQuery

--- funcoes.js
+++ funcoes.js
@@ -1,35 +1,16 @@
-function validarFormulario(formulario) {
-
-    for(i=0; i<formulario.length-2; i++){
-        if(formulario[i].value == ''){
-            return false;
-        }
-    }
-
-    return true;
-
-}
-
-function enviarFormulario(formulario) {   
-
-    var validacao = validarFormulario(formulario);
-
-    if(validacao) {
-        document.querySelector("tbody").innerHTML += `\
-        <tr>\
-        <td>${formulario.nome.value}</td>\
-        <td>${formulario.email.value}</td>\
-        <td>${formulario.telefone.value}</td>\
-        </tr>\
-        `;
-        
-        document.querySelector("table").setAttribute('class', '');
-
-        formulario.reset();
-    }
-    else{
-        alert("Preencha os campos do formulário!");
-    }
-
-    return false;
-}
+$("form").submit(function(event){
+    event.preventDefault();
+    var nome = $("#nome").val();
+    var email = $("#email").val();
+    var telefone = $("#telefone").val();
+    var linha = $("tbody").html();
+    linha += `
+        <tr>
+            <td>${nome}</td>
+            <td>${email}</td>
+            <td>${telefone}</td>
+        </tr>
+    `;
+    $("tbody").html(linha);
+    $("table").show();
+});
